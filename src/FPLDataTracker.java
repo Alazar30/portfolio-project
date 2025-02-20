ba405ca56a40c697b4885c703757d1d3547435cb
Alazar30 <alazaralemayehu30@gmail.com>
2025-02-20 13:37:51 -0500

    Updated java file
    Updated java file. Main method and kernel/secondary methods.

--- src/FPLDataTracker.java
+++ src/FPLDataTracker.java
@@ -16,7 +16,7 @@ public class FPLDataTracker {
     /**
      * Example field representing player data storage.
      */
-    private Map<String, String> playerStats;
+    private Map<String, Map<String, String>> playerStats;
 
     /**
      * Creator of initial representation.
@@ -42,27 +42,51 @@ public class FPLDataTracker {
      * Kernal Methods.
      */
 
-    @Override
-    public final void addPlayer(String name, Map<K, V> stats) {
+    /**
+     * Adds a player to the data tracker.
+     *
+     * @param name
+     * @param stats
+     */
+    public final void addPlayer(String name, Map<String, String> stats) {
         this.playerStats.add(name, stats);
     }
 
-    @Override
+    /**
+     * Removes a player from the data tracker.
+     *
+     * @param name
+     */
     public final void removePlayer(String name) {
         this.playerStats.remove(name);
     }
 
-    @Override
-    public final void updatePlayerStats(String name, Map<K, V> stats) {
+    /**
+     * Updates the player stats for a given player.
+     *
+     * @param name
+     * @param stats
+     */
+    public final void updatePlayerStats(String name,
+            Map<String, String> stats) {
         this.playerStats.replaceValue(name, stats);
     }
 
-    @Override
+    /**
+     * Returns whether a player is in the data tracker.
+     *
+     * @param name
+     * @return boolean
+     */
     public final boolean containsPlayer(String name) {
         return this.playerStats.hasKey(name);
     }
 
-    @Override
+    /**
+     * Returns the number of players in the data tracker.
+     *
+     * @return int
+     */
     public final int getPlayerCount() {
         return this.playerStats.size();
     }
@@ -71,23 +95,43 @@ public class FPLDataTracker {
      * Secondary Methods.
      */
 
-    @Override
-    public final Map.Pair<K, V> getPlayerStats(String name) {
+    /**
+     * Returns the player stats for a given player.
+     *
+     * @param name
+     * @return Map<String, String>
+     */
+    public final Map<String, String> getPlayerStats(String name) {
         return this.playerStats.value(name);
     }
 
-    @Override
-    public final String getTopScorers(String name) {
+    /**
+     * Returns the top scorers in the league.
+     *
+     * @param name
+     * @return Map<String, String>
+     */
+    public final Map<String, String> getTopScorers(String name) {
         return this.playerStats.value(name);
     }
 
-    @Override
-    public final String getTopAssisters(String name) {
+    /**
+     * Returns the top assisters in the league.
+     *
+     * @param name
+     * @return Map<String, String>
+     */
+    public final Map<String, String> getTopAssisters(String name) {
         return this.playerStats.value(name);
     }
 
-    @Override
-    public final String getBestPlayersByPosition(String name) {
+    /**
+     * Returns the best players by position.
+     *
+     * @param name
+     * @return Map<String, String>
+     */
+    public final Map<String, String> getBestPlayersByPosition(String name) {
 
     }
 
@@ -101,19 +145,33 @@ public class FPLDataTracker {
 
         FPLDataTracker tracker = new FPLDataTracker();
 
-        tracker.addPlayer("Alazar", new Map.Pair<>("Goals", "10"));
-        tracker.addPlayer("Hazard", new Map.Pair<>("Goals", "20"));
-        tracker.addPlayer("Palmer", new Map.Pair<>("Goals", "30"));
+        Map<String, String> alazarStats = new Map1L<>();
+        alazarStats.add("Goals", "10");
+        tracker.addPlayer("Alazar", alazarStats);
 
-        out.println(tracker.getPlayerStats("Alazar"));
-        out.println(tracker.getPlayerStats("Hazard"));
-        out.println(tracker.getPlayerStats("Palmer"));
-        out.println(tracker.getPlayerCount());
+        Map<String, String> hazardStats = new Map1L<>();
+        hazardStats.add("Goals", "20");
+        tracker.addPlayer("Hazard", hazardStats);
 
-        tracker.updatePlayerStats("Alazar", new Map.Pair<>("Goals", "15"));
-        out.println(tracker.getPlayerStats("Alazar"));
+        Map<String, String> palmerStats = new Map1L<>();
+        palmerStats.add("Goals", "30");
+        tracker.addPlayer("Palmer", palmerStats);
+
+        out.println("Alazar Stats: " + tracker.getPlayerStats("Alazar"));
+        out.println("Hazard Stats: " + tracker.getPlayerStats("Hazard"));
+        out.println("Palmer Stats: " + tracker.getPlayerStats("Palmer"));
+        out.println("\nTotal Amount of Players: " + tracker.getPlayerCount());
+
+        Map<String, String> alazarNewStats = new Map1L<>();
+        alazarNewStats.add("Goals", "15");
+        tracker.updatePlayerStats("Alazar", alazarNewStats);
+        out.println(
+                "\nUpdated Alazar Stats: " + tracker.getPlayerStats("Alazar"));
 
         tracker.removePlayer("Hazard");
-        out.println(tracker.getPlayerCount());
+        out.println("\nUpdated Total Amount of Players after Removing Hazard: "
+                + tracker.getPlayerCount());
+
+        out.close();
     }
 }
